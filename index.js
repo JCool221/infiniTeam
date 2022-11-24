363c4b3d25bd0b919ef0fb16e2e82a7d3bea9fa0
JCool221 <109809337+JCool221@users.noreply.github.com>
2022-11-23 20:59:56 -0500

    got tests working

--- index.js
+++ index.js
@@ -3,100 +3,9 @@ const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const fs = require('fs');
 const Intern = require('./lib/intern');
+const Employee = require('./lib/employee');
 const theTeam = [];
 
-const managerQuestions = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: "Enter the manager's name",
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: "Enter the manager's ID number",
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "Enter the manager's email address",
-        },   
-        {
-            type: 'input',
-            name: 'office',
-            message: "Enter the manager's office number",
-        },
-    ])
-    .then(managerData => {
-        const { name, id, email, office} = managerData;
-        const manager = new Manager (`Hi there, I'm ${name} id#${id}, my email is ${email} office# ${office}`);
-        theTeam.push(manager);
-        console.log(theTeam);
-        team();
-    })
-};
-
-const team = () => {
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'role',
-            message:"What kind of employee would you like to add?",
-            choices: ['Engineer', 'Intern'] ,
-        },
-        {
-            type: 'input',
-            name: 'name',
-            message: "Enter the employee's name",
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: "Enter the employee's ID number",
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "Enter the employee's email address",
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: "Enter the employee's Github",
-            when: (data) => data.role == 'Engineer',
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: "Enter the employee's school",
-            when: (data) => data.role == 'Intern',
-         },
-         {
-            type: 'confirm',
-            name: 'addNewEmployee',
-            message: 'would you like to add another team member?',
-            default: false,
-         },
-    ])
-    .then(employeeData => {
-        const { role, name, id, email, github, school, addNewEmployee } = employeeData;
-        let employee;
-        if (role==="Engineer") {
-            employee = new Engineer(name, id, email, github);
-            console.log(employee);
-        } else if (role==="Intern") {
-            employee = new Intern (name, id, email, school);
-            console.log(employee);
-        }   theTeam.push(employee);
-        if (addNewEmployee) {
-            return team();
-        } else {
-            console.log(theTeam);
-            // fs.writeFile('Main.html', generateHTML());
-        }
-    })
-};
 
 const generateHTML = () =>
      `<!doctype html>
@@ -116,23 +25,6 @@ const generateHTML = () =>
     </header>
     <div class="container">
         <main id='cardDeck' class="row justify-content-center">
-
-            <div class="col-4">
-                <div class="card  mt-3" style="width: 18rem;">
-                    <div class="card-header bg-info">
-                        <h1 id='role' class="display-4">Test Text</h1>
-                    </div>
-                    <div class="card-body">
-                        <ul class="list-group list-group-flush">
-                            <li id='item-1' class="list-group-item">An item</li>
-                            <li id='item-2' class="list-group-item">A second item</li>
-                            <li id='item-3' class="list-group-item">A third item</li>
-                            <li id='item-4' class="list-group-item">A fourth item</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
         </main>
     </div>
         
@@ -149,9 +41,103 @@ const generateHTML = () =>
 </html>`;
 
 
+const managerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter the manager's name",
+        },    
+        {
+            type: 'input',
+            name: 'id',
+            message: "Enter the manager's ID number",
+        },    
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter the manager's email address",
+        },       
+        {
+            type: 'input',
+            name: 'office',
+            message: "Enter the manager's office number",
+        },    
+    ])    
+    .then(managerData => {
+        const { name, id, email, office} = managerData;
+        const manager = new Manager (name, id, email, office);
+        theTeam.push(manager.printInfo());
+        console.log(theTeam);
+        team();
+    })    
+};    
+
+const team = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message:"What kind of employee would you like to add?",
+            choices: ['Engineer', 'Intern'] ,
+        },    
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter the employee's name",
+        },    
+        {
+            type: 'input',
+            name: 'id',
+            message: "Enter the employee's ID number",
+        },    
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter the employee's email address",
+        },    
+        {
+            type: 'input',
+            name: 'github',
+            message: "Enter the employee's Github",
+            when: (data) => data.role == 'Engineer',
+        },    
+        {
+            type: 'input',
+            name: 'school',
+            message: "Enter the employee's school",
+            when: (data) => data.role == 'Intern',
+         },   
+         {
+            type: 'confirm', 
+            name: 'addNewEmployee',
+            message: 'would you like to add another team member?',
+            default: false,
+         },   
+    ])     
+    .then(employeeData => {
+        const { role, name, id, email, github, school, addNewEmployee } = employeeData;
+        let employee;
+        if (role==="Engineer") {
+            employee = new Engineer(name, id, email, github);
+            console.log(employee);
+        } else if (role==="Intern") {
+            employee = new Intern (name, id, email, school);
+            console.log(employee);
+        }   theTeam.push(employee.printInfo());    
+        if (addNewEmployee) {
+            return team();
+        } else {
+            console.log(theTeam);
+            //fs.writeFile(`main.html`, generateHTML(theTeam));
+        }    
+    })    
+};    
 // Function to initialize app
 const init = () => {
     managerQuestions()
+    // const bob = new Employee('bob', 32, 'bob@bob.com');
+    // bob.printInfo();
 };
 
 // Function call to initialize app
