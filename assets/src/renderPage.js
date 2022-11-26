eeca78f7805d0739dc01409cc179c1953fdbf09f
JCool221 <109809337+JCool221@users.noreply.github.com>
2022-11-26 15:56:01 -0500

    fs writefile working, cards are not

--- assets/src/renderPage.js
+++ assets/src/renderPage.js
@@ -1,25 +1,95 @@
 
-const renderCards = function () {
+const renderManager = function (manager) {
     return `
         <div class="col-4">
             <div class="card  mt-3" style="width: 18rem;">
                 <div class="card-header bg-info">
-                    <h1 id='role' class="display-4">{test.name}</h1>
+                    <h1 id='role' class="display-4">${manager.name}</h1>
                 </div>
                 <div class="card-body">
                     <ul class="list-group list-group-flush">
-                        <li id='item-1' class="list-group-item">{test.id}</li>
-                        <li id='item-2' class="list-group-item">{test.email}</li>
-                        <li id='item-3' class="list-group-item">{test.office}</li>
+                        <li id='item-1' class="list-group-item">${manager.id}</li>
+                        <li id='item-2' class="list-group-item">${manager.email}</li>
+                        <li id='item-3' class="list-group-item">${manager.office}</li>
                     </ul>
                 </div>
             </div>
         </div>
     `;
 };
-const renderPage = generateHTML(renderCards);
 
-function generateHTML() {
+
+const renderEngineer = function (engineer) {
+    return `
+        <div class="col-4">
+            <div class="card  mt-3" style="width: 18rem;">
+                <div class="card-header bg-info">
+                    <h1 id='role' class="display-4">${engineer.name}</h1>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li id='item-1' class="list-group-item">${engineer.id}</li>
+                        <li id='item-2' class="list-group-item">${engineer.email}</li>
+                        <li id='item-3' class="list-group-item">${engineer.office}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    `;
+};
+
+
+const renderIntern = function (intern) {
+    return `
+        <div class="col-4">
+            <div class="card  mt-3" style="width: 18rem;">
+                <div class="card-header bg-info">
+                    <h1 id='role' class="display-4">${intern.name}</h1>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li id='item-1' class="list-group-item">${intern.id}</li>
+                        <li id='item-2' class="list-group-item">${intern.email}</li>
+                        <li id='item-3' class="list-group-item">${intern.office}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    `;
+};
+
+
+renderPage = (data) => {
+    console.log('called renderpage');
+    cardArray = [];
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        if (this.role === 'Manager') {
+            const managerCard = renderManager(employee);
+            cardArray.push(managerCard);
+            console.log('manager');
+        }
+        if (this.role === 'Engineer') {
+            const engineerCard = renderEngineer(employee);
+            cardArray.push(engineerCard);
+            console.log('engineer');
+        }
+        if (this.role === 'Intern') {
+            const internCard = renderIntern(employee);
+            cardArray.push(internCard);
+            console.log('intern');
+        }
+    }
+    cardHTML = cardArray.join('');
+    console.log('created cards');
+    const generatedPage = generateHTML(cardHTML);
+    console.log('generate page');
+    return generatedPage;
+}
+
+const generateHTML = (cardHTML) => {
+    console.log('called generateHTML');
     return `
         <!doctype html>
         <html lang="en">    
@@ -38,7 +108,7 @@ function generateHTML() {
             </header>    
             <div class="container">
                 <main id='cardDeck' class="row justify-content-center">   
-                    ${renderCards()}
+                    ${cardHTML}
                 </main>
             </div>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
